2acd32f801f82954747956ef2cb72ff1362b76b5
ZippyDaFish <andrew.riker21@gmail.com>
2023-04-17 13:42:38 -0400

    Add base file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  
+import React, { Component } from 'react';
+
+import Homepage from './components/homepage.js'
+
+class App extends Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+    return(
+      <div>
+        <Homepage/>
+      </div>
+    );
+  }
 }
 
 export default App;
