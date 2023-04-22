1d8be8673ddbcf8f54ba52d0f0c1e88809a9c4bd
ZippyDaFish <andrew.riker21@gmail.com>
2023-04-22 16:43:39 -0400

    Style homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import TitleBar from './components/TitleBar.js'
 
+import './styles/App.css'
+
 const App = () => {
   return(
     <div>
       <TitleBar/>
-      <h1>Hello Homepage - App</h1>
+      <div className='content'>
+        <div className='tagline-container'>
+          <p className='tagline'>We don't sell a product, we don't sell anything!</p>
+          <a href='/shop'>Shop Now</a>
+        </div>
+        <div className='image-container'>
+          <img alt='this is an image'/>
+        </div>
+      </div>
     </div>
   );
 };
