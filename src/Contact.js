55f4fecab7add47117c344338e92fc5ab1d2549d
ZippyDaFish <andrew.riker21@gmail.com>
2023-04-26 14:22:58 -0400

    Add contact links

--- src/Contact.js
+++ src/Contact.js
@@ -5,7 +5,15 @@ const Contact = () => {
     return(
         <div>
             <TitleBar/>
-            <h1>Hello Contact</h1>
+            <div className="contact-links">
+                <p>Don't contact us on any of these sites</p>
+                <a href="https://github.com/ZippyDaFish/" target='_blank'>Github</a>
+                <a href="https://www.youtube.com/watch?v=Sagg08DrO5U" target='_blank'>Instagram</a>
+                <a href="https://www.youtube.com/watch?v=Sagg08DrO5U" target='_blank'>Facebook</a>
+            </div>
+            <div className="address-container">
+                <p className="address">Real Road 123, Ohio USA</p>
+            </div>
         </div>
     );
 };
