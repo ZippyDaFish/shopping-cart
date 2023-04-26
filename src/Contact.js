635e12f80b1e3b8773c9f38e35b7a11461d48ca7
ZippyDaFish <andrew.riker21@gmail.com>
2023-04-26 15:06:30 -0400

    Add contact styling

--- src/Contact.js
+++ src/Contact.js
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
 import TitleBar from "./components/TitleBar";
 
+import './styles/Contact.css';
+
+import ghIcon from './images/gh-icon.png';
+import igIcon from './images/ig-icon.png';
+import fbIcon from './images/fb-icon.png';
+
 const Contact = () => {
     return(
         <div>
             <TitleBar/>
-            <div className="contact-links">
+            <div className="contact-container">
                 <p>Don't contact us on any of these sites</p>
-                <a href="https://github.com/ZippyDaFish/" target='_blank'>Github</a>
-                <a href="https://www.youtube.com/watch?v=Sagg08DrO5U" target='_blank'>Instagram</a>
-                <a href="https://www.youtube.com/watch?v=Sagg08DrO5U" target='_blank'>Facebook</a>
-            </div>
-            <div className="address-container">
-                <p className="address">Real Road 123, Ohio USA</p>
+                <div className="contact-links">
+                    <a href="https://github.com/ZippyDaFish/" target='_blank'><img src={ghIcon}/></a>
+                    <a href="https://www.youtube.com/watch?v=Sagg08DrO5U" target='_blank'><img src={igIcon}/></a>
+                    <a href="https://www.youtube.com/watch?v=Sagg08DrO5U" target='_blank'><img src={fbIcon}/></a>
+                </div>
+                <div className="address-container">
+                    <p>False-ify</p>
+                    <p className="address">Real Road 123, Ohio USA</p>
+                </div>
             </div>
         </div>
     );
