7f4b196729e676a6ac207b471dbf9e4b75533e91
ZippyDaFish <andrew.riker21@gmail.com>
2023-05-08 19:06:36 -0400

    Add open cart button and styling

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../styles/TitleBar.css'
 
+import cartIcon from '../images/shopping-cart-icon.png';
+
 const TitleBar = () => {
     return(
         <div className='title-bar-container'>
@@ -9,6 +11,7 @@ const TitleBar = () => {
                 <a href='/' className='nav-link'>Home</a>
                 <a href='/shop' className='nav-link'>Shop</a>
                 <a href='/contact' className='nav-link'>Contact</a>
+                <button className='open-cart-btn'><img className='cart-icon' src={cartIcon}/></button>
             </div>
         </div>
     );
